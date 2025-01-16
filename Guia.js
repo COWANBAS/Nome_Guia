1d615f1998b92eacbee5a7fccb25a2d01c6c6ead
Kauan Fonseca <131304556+COWANBAS@users.noreply.github.com>
2025-01-16 12:53:55 -0300

    Add files via upload

---
+++ Guia.js
@@ -0,0 +1,33 @@
+// ==UserScript==
+// @name           UmaParadaAe
+// @description    ZAPZAP
+// @spacename      CowanZAP
+// license         CowBas
+// @version        1.0
+// @author         Cowanbas
+// @match          https://web.whatsapp.com/*
+// @run-at         document-start
+// ==/UserScript==
+
+(function () {
+  'use strict';
+
+  // ZAPZAP
+  const Ozapzap = "MUDA AQUI";
+
+  const Frango = () => {
+    if (document.title !== Ozapzap) {
+      document.title = Ozapzap;
+    }
+  };
+
+  Frango();
+
+  const observer = new MutationObserver(() => {
+    Frango();
+  });
+
+  const config = { subtree: true, childList: true, characterData: true };
+  observer.observe(document.querySelector('head'), config);
+
+})();
