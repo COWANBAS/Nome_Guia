ab8ade7fc136d8071f3223b8d8fad27d9cfc8e5f
Kauan Fonseca <131304556+COWANBAS@users.noreply.github.com>
2025-01-16 18:44:03 -0300

    Add files via upload

---
+++ NomeGuia.js
@@ -0,0 +1,36 @@
+// ==UserScript==
+// @name           Nome Guia
+// @description    Altera o nome da guia
+// @namespace      CowanGUIA
+// @license        CowBas
+// @version        1.0
+// @author         Cowanbas
+// @match          https://web.whatsapp.com/*
+// @run-at         document-start
+// ==/UserScript==
+
+// Mude ou adicione no @match o site que deseja alterar o nome da guia
+
+(function () {
+  'use strict';
+
+  // Altere o nome da guia aqui
+  const NomeGuia = "MUDE AQUI";
+
+  // Alterar o nome da guia para o valor de "NomeGuia"   
+  const Wpp = () => {
+    if (document.title !== NomeGuia) {
+      document.title = NomeGuia;
+    }
+  };
+
+  Wpp();
+  const observer = new MutationObserver(() => {
+    Wpp();
+  });
+
+  // Observar mudanças no título da guia
+  const config = { subtree: true, childList: true, characterData: true };
+  observer.observe(document.querySelector('head'), config);
+
+})();
